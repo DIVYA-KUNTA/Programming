9e676b590a70125a713359f0a69e5e8c2914c858
DIVYA-KUNTA <DIVYA-KUNTA>
2024-01-25 02:34:46 +0000

    Deleted queries that are copied to section-4

--- Advanced_SQL_Course/Copy_Divya_Kunta_SQL_Queries.sql
+++ Advanced_SQL_Course/Copy_Divya_Kunta_SQL_Queries.sql
@@ -25,24 +25,3 @@ GROUP BY
 ORDER BY
 	sessions DESC
 
-
--- COMMAND ----------
-
--- MAGIC %md
--- MAGIC ## Query_2
-
--- COMMAND ----------
-
-SELECT
-	COUNT(DISTINCT website_sessions.website_session_id) AS sessions,
-    COUNT(DISTINCT order_id) AS orders,
-   COUNT(DISTINCT order_id)/ COUNT(DISTINCT website_sessions.website_session_id) AS session_to_order_conv_rate
-FROM
-	website_sessions
-LEFT JOIN orders
-	ON website_sessions.website_session_id = orders.website_session_id
-WHERE
-	website_sessions.created_at < '2012-04-14'
-	AND utm_source = 'gsearch'
-    AND utm_campaign = 'nonbrand'
-    
